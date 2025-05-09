d7a9f07193cc1364707501e418f2f109e67cfce3
@ELTOROIT - Andres Perez <andresperezf@yahoo.com>
2025-05-09 17:18:54 -0400

    Metadata ready for packaging

--- force-apps/dreamhouse_ELTOROit/main/default/lwc/sampleDataImporter/sampleDataImporter.js
+++ force-apps/dreamhouse_ELTOROit/main/default/lwc/sampleDataImporter/sampleDataImporter.js
@@ -1,25 +1,32 @@
-import { LightningElement } from 'lwc';
-import { ShowToastEvent } from 'lightning/platformShowToastEvent';
-import importSampleData from '@salesforce/apex/SampleDataController.importSampleData';
+import { LightningElement } from "lwc";
+import { ShowToastEvent } from "lightning/platformShowToastEvent";
+import importSampleData from "@salesforce/apex/SampleDataController.importSampleData";
 
 export default class SampleDataImporter extends LightningElement {
-    handleImportSampleData() {
-        importSampleData()
-            .then(() => {
-                const evt = new ShowToastEvent({
-                    title: 'Success',
-                    message: 'Sample data successfully imported',
-                    variant: 'success'
-                });
-                this.dispatchEvent(evt);
-            })
-            .catch((e) => {
-                const evt = new ShowToastEvent({
-                    title: 'Error while importing data',
-                    message: e.message,
-                    variant: 'error'
-                });
-                this.dispatchEvent(evt);
-            });
-    }
+	buttonDisabled = false;
+
+	handleImportSampleData() {
+		this.buttonDisabled = true;
+		setTimeout(() => {
+			importSampleData()
+				.then(() => {
+					const evt = new ShowToastEvent({
+						title: "Success",
+						message: "Sample data successfully imported",
+						variant: "success"
+					});
+					this.dispatchEvent(evt);
+					this.buttonDisabled = false;
+				})
+				.catch((e) => {
+					const evt = new ShowToastEvent({
+						title: "Error while importing data",
+						message: e.message,
+						variant: "error"
+					});
+					this.dispatchEvent(evt);
+					this.buttonDisabled = false;
+				});
+		}, 1e2);
+	}
 }
